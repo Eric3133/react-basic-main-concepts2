c4503fbb35f1a9bda08f4a35dd036b8eefdce37a
mahongri <mahongri1993@gmail.com>
2024-05-10 23:42:55 -0400

    useref to get dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useRef } from 'react';
 
 function App() {
+  const [data, setData] = useState('');
+
+  // get DOM element(useRef)
+  const inputRef = useRef(null);
+  const showDom = () => {
+    console.log(inputRef.current.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App</h1>
+      <div>
+        <input
+          type = "text"
+          value = {data}
+          onChange = {(e) => setData(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <input
+          type = "text"
+          ref = {inputRef}
+        />
+        <button onClick = {showDom}>get DOM</button>
+      </div>
     </div>
   );
 }
